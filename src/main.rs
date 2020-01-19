715686a9786261e38f8bb61dad2edfcf7d43ec53
Flakebi <flakebi@t-online.de>
2020-01-19 20:41:58 +0100

    Make code more beautiful

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,35 @@
-//#[macro_use] extern crate failure;
-//extern crate ring;
-extern crate tsproto;
-extern crate rayon;
-use structopt::StructOpt;
-
-use tsproto::crypto::{EccKeyPrivP256, EccKeyPubP256};
 use rayon::prelude::*;
-use std::thread;
+use structopt::StructOpt;
+use tsproto::crypto::{EccKeyPrivP256, EccKeyPubP256};
 
 #[derive(StructOpt, Debug)]
+#[structopt(about, author)]
 struct Opts {
-    /// Sets a custom config file. Could have been an Option<T> with no default too
-    #[structopt(short = "p", long = "pattern")]
-    pattern: Vec<String>,
+	/// Sets a custom config file. Could have been an Option<T> with no default too
+	#[structopt()]
+	pattern: Vec<String>,
 }
 
 fn main() {
 	let opts: Opts = Opts::from_args();
 
-	let patt = opts.pattern.into_iter().map(|p| p.into_bytes()).collect();
+	if opts.pattern.is_empty() {
+		println!("No patters given, please call with wanted uid strings");
+		return;
+	}
+
+	let patt = opts.pattern.into_iter().map(|p| p.into_bytes()).collect::<Vec<_>>();
 	gen_bench_para(&patt);
 }
 
-fn gen_single(patt: &Vec<Vec<u8>>) -> bool {
+fn gen_single(patt: &[Vec<u8>]) -> bool {
 	let tp_priv = EccKeyPrivP256::create().unwrap();
 	let tp_pub: EccKeyPubP256 = (&tp_priv).into();
 	let uid = tp_pub.get_uid_no_base64().unwrap();
 	for p in patt {
 		if uid.starts_with(p) {
 			let export = tp_priv.to_ts().unwrap();
-			println!("MATCH {} KEY: {}", String::from_utf8(p.to_vec()).unwrap(), export);
+			println!("MATCH {} KEY: {} UID: {}", String::from_utf8(p.to_vec()).unwrap(), export, tp_pub.get_uid().unwrap());
 			return true;
 		}
 	}
@@ -48,15 +48,15 @@ fn gen_single(patt: &Vec<Vec<u8>>) -> bool {
 // 	}
 // }
 
-fn gen_bench_para(patt: &Vec<Vec<u8>>) {
+fn gen_bench_para(patt: &[Vec<u8>]) {
 	//rayon::ThreadPoolBuilder::new().num_threads(12).build_global().unwrap();
 	let mut found_any = false;
 	while !found_any {
-		found_any = (0..500000).into_par_iter().any(|_|{
+		found_any = (0..500_000).into_par_iter().any(|_| {
 			gen_single(patt)
 		});
 	}
-	println!("done");
+	println!("Done");
 }
 
 // fn gen_bench_para_man(patt: Vec<String>) {
